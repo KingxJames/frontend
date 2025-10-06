602b34d724b18922dacabfbe7b3613158e250b31
KingxJames <james.faber@ub.edu.bz>
2025-10-06 16:58:58 -0600

    asdf

--- src/common/crudTables/incidentReportsTable.tsx
+++ src/common/crudTables/incidentReportsTable.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid";
 import {
   IconButton,
   Button,
@@ -112,7 +112,8 @@ export const IncidentReportTable: React.FC = () => {
   );
 
   const handlePreview = (incidentReport: IIncidentReport) => {
-    console.log(incidentReport);
+    setSelectedIncidentReport(incidentReport); // store the report to preview
+    setOpenPreview(true); // open the dialog
   };
 
   const handleDownload = (incidentReport: IIncidentReport) => {
@@ -184,21 +185,224 @@ export const IncidentReportTable: React.FC = () => {
       <Dialog
         open={openPreview}
         onClose={() => setOpenPreview(false)}
-        fullScreen
+        fullWidth
+        maxWidth="md"
       >
-        <DialogTitle>Preview Incident Report</DialogTitle>
+        <DialogTitle
+          sx={{
+            mb: 2,
+            color: "#5E4B8B",
+            fontWeight: "bold",
+            borderBottom: "2px solid #C5A645",
+            pb: 1,
+            textAlign: "center",
+            fontSize: "1.5rem",
+            width: "90%",
+            margin: "auto",
+          }}
+        >
+          Incident Report Preview - {selectedIncidentReport?.caseNumber ?? ""}
+        </DialogTitle>
         <DialogContent>
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            <Grid xs={6}>
-              <Typography variant="h6">Case Number</Typography>
-              <Typography variant="h6">{incidentReports.caseNumber}</Typography>
-            </Grid>
-          </Grid>
+          {selectedIncidentReport && (
+            <Box sx={{ flexGrow: 1, mt: 2, mb: 2 }}>
+              <Grid container spacing={2} sx={{ p: "2%" }}>
+                {/* Date */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Date"
+                    fullWidth
+                    value={selectedIncidentReport.date}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Time */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Time"
+                    fullWidth
+                    value={selectedIncidentReport.time ?? ""}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Building Name */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Building Name"
+                    fullWidth
+                    value={selectedIncidentReport.buildingName}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Campus */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Campus"
+                    fullWidth
+                    value={selectedIncidentReport.campus}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Incident Type */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Incident Type"
+                    fullWidth
+                    value={selectedIncidentReport.incidentType}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Status */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Status"
+                    fullWidth
+                    value={selectedIncidentReport.incidentReportStatus}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Description */}
+                <Grid item xs={12}>
+                  <TextField
+                    label="Description of Incident"
+                    fullWidth
+                    multiline
+                    rows={4}
+                    value={selectedIncidentReport.description}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* upload files - Fix this*/}
+                {/* <Grid item xs={12}>
+                  <TextField
+                    label="Uploaded Files"
+                    fullWidth
+                    multiline
+                    rows={3}
+                    value={selectedIncidentReport.incidentFiles?.length ?? 0}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid> */}
+
+                <Box sx={{ width: "100%", pl: 2 }}>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      mt: 2,
+                      mb: 1,
+                      fontWeight: "bold",
+                      color: "#5E4B8B",
+                      borderBottom: "2px solid #C5A645",
+                      width: "100%",
+                      pb: 1,
+                    }}
+                  >
+                    Incident Files
+                  </Typography>
+                  <Grid container spacing={2} sx={{ p: "2%" }}>
+                    {selectedIncidentReport.incidentFiles?.map(
+                      (file: IIncidentFile, index: number) => (
+                        <Grid item xs={12} md={6} key={index}>
+                          <TextField
+                            label={`Incident File ${index + 1}`}
+                            fullWidth
+                            value={file.incidentPicture}
+                            InputProps={{ readOnly: true }}
+                          />
+                        </Grid>
+                      )
+                    )}
+                  </Grid>
+                </Box>
+
+                {/* Action Taken */}
+                <Grid item xs={12}>
+                  <TextField
+                    label="Action Taken"
+                    fullWidth
+                    multiline
+                    rows={3}
+                    value={selectedIncidentReport.action}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                <Box sx={{ width: "100%", pl: 2 }}>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      mt: 2,
+                      mb: 1,
+                      fontWeight: "bold",
+                      color: "#5E4B8B",
+                      borderBottom: "2px solid #C5A645",
+                      width: "100%",
+                      pb: 1,
+                    }}
+                  >
+                    People Involved
+                  </Typography>
+                </Box>
+
+                {/* Reported By */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Reported By"
+                    fullWidth
+                    value={selectedIncidentReport.reportedBy}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Contact */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Contact"
+                    fullWidth
+                    value={selectedIncidentReport.contact}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Witnesses */}
+                <Grid item xs={6} md={6}>
+                  <TextField
+                    label="Witnesses"
+                    fullWidth
+                    value={selectedIncidentReport.witnesses}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+
+                {/* Uploaded By */}
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    label="Uploaded By"
+                    fullWidth
+                    value={selectedIncidentReport.uploadedBy}
+                    InputProps={{ readOnly: true }}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+          )}
         </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => setOpenPreview(false)}
+            variant="outlined"
+            color="secondary"
+          >
+            Close
+          </Button>
+        </DialogActions>
       </Dialog>
     </Box>
   );
